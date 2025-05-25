b23b771a77319e52ccb83d1c11f373452636b87d
vladymyrkryvtsov <kryvtsov.v@gmail.com>
2025-05-25 18:23:19 -0400

    Routes grouping

---
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+function Page() {
+  return (
+    <div>
+      <h1>Dashboard users</h1>
+      <ul className="mt-10">
+        <li>
+          <Link href="/app/(dashboard)/dashboard/users/1">User 1</Link>
+        </li>
+        <li>
+          <Link href="/app/(dashboard)/dashboard/users/2">User 2</Link>
+        </li>
+        <li>
+          <Link href="/app/(dashboard)/dashboard/users/3">User 3</Link>
+        </li>
+        <li>
+          <Link href="/app/(dashboard)/dashboard/users/4">User 4</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Page;
